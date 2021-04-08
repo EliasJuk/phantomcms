2daefc973ec21e524fb51ccc3e7c01e5f5348cd2
EliasJuk <eliasp.juk@hotmail.com>
2021-04-07 22:33:15 -0300

    update config

--- website/config.php
+++ website/config.php
@@ -1,7 +1,19 @@
 <?php
+  session_start();
+
+  # CLASS
+  $autoload = function($class){
+    include('classes/'.$class.'.php');
+  };
+  spl_autoload_register($autoload);
+
   # DATABASE
   define('HOST','db');
   define('USER','root');
   define('PASSWORD','root');
   define('DATABASE','phantomcms');
+
+  # ROUTES
+  define('INCLUDE_PATH', 'http://localhost/');
+  define('INCLUDE_PATH_ADMIN', INCLUDE_PATH.'admin');
 ?>
