30ad83a430d1fef630f85b3c8e16d44bbf7248f7
EliasJuk <eliasp.juk@hotmail.com>
2021-04-07 22:14:47 -0300

    connection and query by pdo

--- website/index.php
+++ website/index.php
@@ -1,3 +1,20 @@
 <?php
-  echo "Hello World!"
+  # ARQUIVO CONTENDO AS VARIAVEIS DE CONEXAO
+  include('config.php');
+
+  # CLASSE DE CONEXAO
+  include('classes/MySql.php');
+
+  # GERA A ESTRUTURA INICIAL DO BANCO DE DADOS
+  Generate::generate_database();
+?>
+
+<?php
+  # CONSULTA OS USUARIOS DENTRO DO BANCO DE DADOS
+  $sql = MySql::conectar()->prepare("SELECT * from `tb.admin_users`");
+  $sql->execute();
+
+  while ($line = $sql->fetch(PDO::FETCH_ASSOC)) {
+    echo "Usuario: {$line['user']} | Senha: {$line['password']}<br>";
+  }
 ?>
